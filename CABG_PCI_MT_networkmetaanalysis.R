3223876e51682fd66ef373941a30d52fe418b18e
anupamsingh81 <anupampom@gmail.com>
2017-08-19 01:48:21 +0530

    hhhi

--- CABG_PCI_MT_networkmetaanalysis.R
+++ CABG_PCI_MT_networkmetaanalysis.R
@@ -72,4 +72,36 @@ expTheta<-exp(thetaSummaryObs)
 print(thetaSummaryObs)
 print(expTheta)
 
-
+x<-thetaSummaryObs$mean
+y<-c(1,2,3)
+plot(x,y,xlim=c(-0.7,1.7),ylim=c(3.5,0),pch=23,cex=4,ylab="",yaxt="n",col="black",bg="lightblue",
+     cex.axis=1.0, xlab="log(e)OR", cex.lab=1.6)
+axis (4, pos=0.0, tck = 0, labels=FALSE, col="black")
+text (-0.555,1,"CABG vs. PCI", cex= 1.4)
+text (-0.53,3,"CABG vs. MT",cex = 1.4)
+text (-0.54,2, "PCI mos vs. MT", cex = 1.4)
+text (0, 0,"All-Cause Mortality",cex = 1.6,font =2)
+text (0.4, 0.6, "CABG Better",cex=1.6,font=3)
+text (-0.4, 0.6, "PCI Better",cex=1.6,font=3)
+text (thetaSummaryObs$mean[3], 3.2, font=2, round(expTheta$mean[3],2))
+text (thetaSummaryObs$val2.5pc[3], 3.2, font=2,round(expTheta$val2.5pc[3],2))
+#text (thetaSummaryObs$val97.5pc[3], 3.2, font=2,round(expTheta$val97.5pc[3],2))
+text (thetaSummaryObs$val97.5pc[3], 3.2, font=2,"1.60")
+text (thetaSummaryObs$mean[1],1.2,font=2,round(expTheta$mean[1],2))
+#text (thetaSummaryObs$val2.5pc[1], 1.2, font=2,round(expTheta$val2.5pc[1],2))
+text (thetaSummaryObs$val2.5pc[1], 1.2, font=2,"0.70")
+text (thetaSummaryObs$val97.5pc[1], 1.2, font=2,round(expTheta$val97.5[1],2))
+text (thetaSummaryObs$mean[2], 2.2, font=2,round(expTheta$mean[2],2))
+text (thetaSummaryObs$val2.5pc[2], 2.2, font=2,round(expTheta$val2.5pc[2],2))
+text (thetaSummaryObs$val97.5pc[2], 2.2, font=2,round(expTheta$val97.5pc[2],2))
+segments(thetaSummaryObs$val2.5pc[3], 3, thetaSummaryObs$mean[3]-0.025, 3, lty=1, col="black", lwd=3)
+segments(thetaSummaryObs$val97.5pc[3], 3, thetaSummaryObs$mean[3]+0.025, 3, lty=1, col="black", lwd=3)
+segments(thetaSummaryObs$val2.5pc[1], 1, thetaSummaryObs$mean[1]-0.025, 1, lty=1, lwd=3)
+segments(thetaSummaryObs$val97.5pc[1], 1, thetaSummaryObs$mean[1]+0.025, 1, lty=1, lwd=3)
+segments(thetaSummaryObs$val2.5pc[2], 2, thetaSummaryObs$mean[2]-0.025, 2, lty=1, lwd=3)
+segments(thetaSummaryObs$val97.5pc[2], 2, thetaSummaryObs$mean[2]+0.025, 2, lty=1, lwd=3)
+mtext ("Posterior Odds Ratio (OR)",3, line =2, cex = 1.6)
+axis (3, at=c(-0.91,-0.69, -0.51,-0.35, -0.22, -0.105,0.0, 0.095,0.182, 0.262,0.336,
+              0.405,0.47,0.531,0.588,0.693, 0.833, 0.956, 1.10,1.19, 1.281,1.386,1.46,1.53,1.61,1.67,1.72,1.79),
+      labels=c(0.4,0.5,0.6, 0.7, 0.8,0.9, "1.0", 1.1,1.2, 1.3,1.4,1.5, 1.6,1.7, 1.8, "2.0", 2.3, 2.6, "3.0",
+               3.3,3.6,"4.0",4.3,4.6,"5.0",5.3,5.6,"6.0"))
